70733f2cc84877084d599305c5f4d98eed0595e3
yabenatti <min.benatti@gmail.com>
2018-10-15 20:21:51 -0300

    Initial Commit

---
+++ MeetupMovile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeetupMovile
+//
+//  Created by Yasmin Benatti on 2018-10-15.
+//  Copyright © 2018 Yasmin Benatti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MeetupMovileTests/MeetupMovileTests.swift
@@ -0,0 +1,34 @@
+//
+//  MeetupMovileTests.swift
+//  MeetupMovileTests
+//
+//  Created by Yasmin Benatti on 2018-10-15.
+//  Copyright © 2018 Yasmin Benatti. All rights reserved.
+//
+
+import XCTest
+@testable import MeetupMovile
+
+class MeetupMovileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
